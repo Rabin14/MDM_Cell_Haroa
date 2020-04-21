e6357aa0a2c7b7c8d432311e8606c270af52f45c
Rabin14 <rabinpathak68@gmail.com>
2020-04-21 18:45:05 +0530

    ok

---
+++ app/src/main/java/block/mdmcellharoa/SplashActivity.java
@@ -0,0 +1,28 @@
+package block.mdmcellharoa;
+import android.content.Intent;
+import android.os.Handler;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, PhoneRegister.class);
+                startActivity(intent);
+                finish();
+            }
+        },2500);
+    }
+}
