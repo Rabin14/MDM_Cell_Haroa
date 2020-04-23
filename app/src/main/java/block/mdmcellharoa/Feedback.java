fbe92d2b5dd5e8dc9ef101cf2876e2a30912d966
Rabin14 <rabinpathak68@gmail.com>
2020-04-23 15:22:14 +0530

    ok

--- app/src/main/java/block/mdmcellharoa/Feedback.java
+++ app/src/main/java/block/mdmcellharoa/Feedback.java
@@ -1,14 +1,195 @@
 package block.mdmcellharoa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import android.os.Bundle;
 
-public class Feedback extends AppCompatActivity {
+public class Feedback extends AppCompatActivity implements View.OnClickListener {
+    TextView school,gp,name;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    EditText subject,feedback,;
+    Button buttonAddItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        school    = (TextView) findViewById(R.id.school);
+
+        gp = (TextView) findViewById(R.id.gp);
+        name = (TextView) findViewById(R.id.name);
+        subject  = (EditText) findViewById(R.id.subject);
+        feedback  = (EditText) findViewById(R.id.feedback);
+
+
+
+        buttonAddItem = (Button)findViewById(R.id.btn_add_item);
+        buttonAddItem.setOnClickListener(this);
+
+
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                school.setText(documentSnapshot.getString("School"));
+
+                gp.setText(documentSnapshot.getString("GPName"));
+                name.setText(documentSnapshot.getString("Name"));
+
+            }
+        });
+
+
+    }
+
+    //This is the part where data is transafeered from Your Android phone to Sheet by using HTTP Rest API calls
+
+    private void   addItemToSheet() {
+
+        final ProgressDialog loading = ProgressDialog.show(this,"Adding Item","Please wait");
+        final String school1 = school.getText().toString().trim();
+
+        final String gp1 = gp.getText().toString().trim();
+
+        final String name1 = name.getText().toString().trim();
+        final String subject1 = subject.getText().toString().trim();
+        final String feedback1 = feedback.getText().toString().trim();
+
+
+
+
+
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbzyjhMoaRNtTI45HS3eJxShXv9VfYOmyJQQrFyGKouoGFSy01vM/exec",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        loading.dismiss();
+                        Toast.makeText(Feedback.this,response,Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> parmas = new HashMap<>();
+
+                //here we pass params
+                parmas.put("action","addItem");
+                parmas.put("school",school1);
+
+                parmas.put("gp",gp1);
+                parmas.put("name",name1);
+                parmas.put("subject",subject1);
+                parmas.put("feedback",feedback1);
+
+
+
+
+                return parmas;
+            }
+        };
+
+        int socketTimeOut = 50000;// u can change this .. here it is 50 seconds
+
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(retryPolicy);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        queue.add(stringRequest);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        if(v==buttonAddItem){
+            addItemToSheet();
+
+            //Define what to do when button is clicked
+        }
     }
 }
