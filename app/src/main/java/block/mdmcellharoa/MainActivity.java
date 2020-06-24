041c56948ec3c2f11dd258e8fc4777a2330ed3e0
Rabin14 <rabinpathak68@gmail.com>
2020-06-24 20:08:48 +0530

    ok

--- app/src/main/java/block/mdmcellharoa/MainActivity.java
+++ app/src/main/java/block/mdmcellharoa/MainActivity.java
@@ -15,6 +15,8 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -27,10 +29,16 @@ import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    TextView fullName, school, dateText, dateText2, dateText3, dise;
+    TextView fullName, school, dateText, dateText2, dateText3,demototal,demodistri,dise,demototal1,demodistri1;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
+    public static final String TAG = "TAG";
+    private static final String KEY_DATE = "date";
+    private static final String KEY_TOTAL = "total";
+    private static final String KEY_PRESENT = "present";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +46,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.setTitle("MDM, Haroa Block");
+        demototal  = (TextView) findViewById(R.id.demototal);
+        demodistri  = (TextView) findViewById(R.id.demodistri);
 
+        demototal1  = (TextView) findViewById(R.id.demototal1);
+        demodistri1  = (TextView) findViewById(R.id.demodistri1);
         dise = findViewById(R.id.dise);
         school = findViewById(R.id.schoolname);
         fullName = findViewById(R.id.profileName);
@@ -50,16 +62,18 @@ public class MainActivity extends AppCompatActivity {
         //set it as current date.
         dateText.setText(date_n);
 
-       // Get the value of shared preference back
-         SharedPreferences getShared = getSharedPreferences("demo2", MODE_PRIVATE);
+       // Get the value of date from firebase back
+
+       /**  SharedPreferences getShared = getSharedPreferences("demo2", MODE_PRIVATE);
          String value = getShared.getString("str2","attendance");
          dateText2.setText(value);
-
+*/
         // Get the value of shared preference back
-        SharedPreferences getShared2 = getSharedPreferences("demo3", MODE_PRIVATE);
+
+        /**SharedPreferences getShared2 = getSharedPreferences("demo3", MODE_PRIVATE);
         String value2 = getShared2.getString("str2", "coverage");
         dateText3.setText(value2);
-
+*/
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
@@ -73,6 +87,9 @@ public class MainActivity extends AppCompatActivity {
                 school.setText(documentSnapshot.getString("School"));
                 fullName.setText(documentSnapshot.getString("Name"));
                 dise.setText(documentSnapshot.getString("dise"));
+                loadNote();
+                loadNote2();
+
 
             }
         });
@@ -146,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
     public void attendance(View view) {
         final String dateTextn = dateText.getText().toString().trim();
         final String dateTextnn = dateText2.getText().toString().trim();
-
+        aladyreenter2();
         if (dateTextn.matches(dateTextnn)) {
             Toast.makeText(MainActivity.this, " Attendance already added Today ! ", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(getApplicationContext(), Piechart_Attendance.class);
@@ -183,9 +200,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void coverage(View view) {
+
         final String dateTextn1 = dateText.getText().toString().trim();
         final String dateTextnn1 = dateText3.getText().toString().trim();
-
+        aladyreenter();
         if (dateTextn1.matches(dateTextnn1)) {
             Toast.makeText(MainActivity.this, " Coverage already added For Today ! ", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(getApplicationContext(), Piechart_Coverage.class);
@@ -209,6 +227,105 @@ public class MainActivity extends AppCompatActivity {
     public void notification(View view) {
         Intent intent = new Intent(getApplicationContext(), ListItem.class);
         startActivity(intent);
+
+
+    }
+    private void loadNote() {
+
+        String userID2 = dise.getText().toString();
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference noteRef = db.collection("coverage").document(userID2);
+        noteRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            String date = documentSnapshot.getString(KEY_DATE);
+                            String total = documentSnapshot.getString(KEY_TOTAL);
+                            String present = documentSnapshot.getString(KEY_PRESENT);
+                            //Map<String, Object> note = documentSnapshot.getData();
+                            dateText3.setText(date);
+                            demototal.setText(total);
+                            demodistri.setText(present);
+
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Enter Your Coverage", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+    private void aladyreenter() {
+
+        String totalp = demototal.getText().toString();
+        String distrip = demodistri.getText().toString();
+        SharedPreferences shrd = getSharedPreferences("demo3", MODE_PRIVATE);
+        SharedPreferences.Editor editor = shrd.edit();
+
+
+        editor.putString("str3", totalp);
+        editor.putString("str4", distrip);
+
+        editor.apply();
+
+
+
+    }
+    private void loadNote2() {
+
+        String userID2 = dise.getText().toString();
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference noteRef = db.collection("attendance").document(userID2);
+        noteRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            String date = documentSnapshot.getString(KEY_DATE);
+                            String total = documentSnapshot.getString(KEY_TOTAL);
+                            String present = documentSnapshot.getString(KEY_PRESENT);
+                            //Map<String, Object> note = documentSnapshot.getData();
+                            dateText2.setText(date);
+                            demototal1.setText(total);
+                            demodistri1.setText(present);
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Enter Your Attendance", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+    private void aladyreenter2() {
+
+        String totalp = demototal1.getText().toString();
+        String distrip = demodistri1.getText().toString();
+
+        SharedPreferences shrd = getSharedPreferences("demo2", MODE_PRIVATE);
+        SharedPreferences.Editor editor = shrd.edit();
+
+
+        editor.putString("str3", totalp);
+        editor.putString("str4", distrip);
+
+        editor.apply();
 
 
     }
